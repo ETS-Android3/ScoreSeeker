5982718ce496596000c7d725e910c60abbda2d95
WiLLiaM-noD <williamkurniawan1144@gmail.com>
2021-05-02 14:37:08 +0700

    Add Detail Activity

--- app/src/main/java/com/william/scoreseeker/MainActivity.java
+++ app/src/main/java/com/william/scoreseeker/MainActivity.java
@@ -1,13 +1,17 @@
 package com.william.scoreseeker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+
+import maes.tech.intentanim.CustomIntent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +20,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+        CardView matchCardView = findViewById(R.id.matchCardView);
+        matchCardView.setOnClickListener(e -> {
+            Intent i = new Intent(MainActivity.this, DetailActivity.class);
+            startActivity(i);
+            CustomIntent.customType(MainActivity.this, "left-to-right");
+        });
     }
 }

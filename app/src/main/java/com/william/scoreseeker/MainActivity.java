625db89207541764b8f3b17671608c5886a6c952
WiLLiaM-noD <williamkurniawan1144@gmail.com>
2021-05-10 17:34:37 +0700

    Testing API for Home Activity

--- app/src/main/java/com/william/scoreseeker/MainActivity.java
+++ app/src/main/java/com/william/scoreseeker/MainActivity.java
@@ -11,6 +11,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.AuthFailureError;
@@ -21,15 +22,27 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.william.scoreseeker.model.Pertandingan;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.lang.reflect.Array;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 import maes.tech.intentanim.CustomIntent;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<Pertandingan> pertandingan = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,19 +50,58 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
 
-        final TextView nameTeam1 = (TextView) findViewById(R.id.nameTeam1);
+        String now = getDate();
+        String yesterday = getYesterday();
+        Log.i("DATE", "onCreate: " + now);
+        Log.i("DATE", "onCreate: " + yesterday);
 
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "https://api.football-data.org/v2/matches/";
+        String url = "https://api.football-data.org/v2/matches/?competitions=PL,SA,PD,FL1&status=FINISHED&dateFrom=" + yesterday + "&dateTo=" + now;
 
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
                 (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
 
                     @Override
                     public void onResponse(JSONObject response) {
-                        Log.i("TAG", "onResponse: " + response);
-                        nameTeam1.setText(response.toString());
+                        JSONArray fetchArray;
+                        JSONObject single, ht, at, score, fulltime;
+                        try {
+                            fetchArray = response.getJSONArray("matches");
+                            Log.i("TAG", "onResponse: " + fetchArray);
+                            for (int i = 0; i < 1; i++) {
+                                single = fetchArray.getJSONObject(i);
+                                ht = single.getJSONObject("homeTeam");
+                                at = single.getJSONObject("awayTeam");
+
+                                score = single.getJSONObject("score");
+                                fulltime = score.getJSONObject("fullTime");
+
+                                Pertandingan p = new Pertandingan();
+                                p.setTimKandang((String) ht.get("name"));
+                                p.setTimTandang((String) at.get("name"));
+                                p.setSkorKandang(String.valueOf(fulltime.get("homeTeam")));
+                                p.setSkorTandang(String.valueOf(fulltime.get("awayTeam")));
+                                pertandingan.add(p);
+                                Log.i("TAG", "onResponse: " + ht.get("name") + " & " + at.get("name"));
+                            }
+
+                            final TextView nameTeam1 = (TextView) findViewById(R.id.nameTeam1);
+                            final TextView nameTeam2 = (TextView) findViewById(R.id.nameTeam2);
+                            final TextView skor1 = (TextView) findViewById(R.id.score1);
+                            final TextView skor2 = (TextView) findViewById(R.id.score2);
+                            final ImageView logo1 = (ImageView) findViewById(R.id.logo1);
+                            final ImageView logo2 = (ImageView) findViewById(R.id.logo2);
+
+                            nameTeam1.setText(pertandingan.get(0).getTimKandang());
+                            nameTeam2.setText(pertandingan.get(0).getTimTandang());
+                            skor1.setText(pertandingan.get(0).getSkorKandang());
+                            skor2.setText(pertandingan.get(0).getSkorTandang());
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }, new Response.ErrorListener() {
                     @Override
@@ -60,12 +112,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public Map<String, String> getHeaders() throws AuthFailureError {
                 HashMap<String, String> headers = new HashMap<String, String>();
-                headers.put("X-Auth-Token", BuildConfig.ApiKey);
+                headers.put("X-Auth-Token", getString(R.string.key));
                 return headers;
             }
         };
-
         queue.add(jsonObjectRequest);
+
 
         CardView matchCardView = findViewById(R.id.matchCardView);
         matchCardView.setOnClickListener(e -> {
@@ -73,5 +125,18 @@ public class MainActivity extends AppCompatActivity {
             startActivity(i);
             CustomIntent.customType(MainActivity.this, "left-to-right");
         });
+    }
+
+    private String getDate() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDateTime now = LocalDateTime.now();
+        return dtf.format(now);
+    }
+
+    private String getYesterday(){
+        Calendar yesterday = Calendar.getInstance();
+        yesterday.add(Calendar.DATE, -1);
+        DateFormat dtf = new SimpleDateFormat("yyyy-MM-dd");
+        return dtf.format(yesterday.getTime());
     }
 }

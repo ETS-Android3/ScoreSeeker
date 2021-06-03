196cd16686be4f194598e42d76e971803966473c
WiLLiaM-noD <williamkurniawan1144@gmail.com>
2021-06-03 18:15:38 +0700

    Get League Data

--- app/src/main/java/com/william/scoreseeker/KlasmenActivity.java
+++ app/src/main/java/com/william/scoreseeker/KlasmenActivity.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -18,6 +19,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.MessageFormat;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -40,20 +42,34 @@ public class KlasmenActivity extends AppCompatActivity {
     }
 
     private void getKlasmenData() {
-        ImageView[] logo = new ImageView[5];
-        logo[0] = findViewById(R.id.logoKlas1);
-        logo[1] = findViewById(R.id.logoKlas2);
-        logo[2] = findViewById(R.id.logoKlas3);
-        logo[3] = findViewById(R.id.logoKlas4);
-        logo[4] = findViewById(R.id.logoKlas5);
+        ImageView[] logos = new ImageView[5];
+        TextView[] detail = new TextView[5];
+        TextView[] win = new TextView[5];
+        TextView[] draw = new TextView[5];
+        TextView[] lose = new TextView[5];
+        TextView[] pts = new TextView[5];
+
         String url = "https://api.football-data.org/v2/competitions/2021/standings";
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, response -> {
             try {
                 JSONArray teamData = response.getJSONArray("standings").getJSONObject(0).getJSONArray("table");
                 for (int i = 0; i < 5; i++) {
+                    logos[i] = (ImageView) findViewById(getResources().getIdentifier( MessageFormat.format("logoKlas{0}", i), "id", getPackageName()));
+                    detail[i] = (TextView) findViewById(getResources().getIdentifier( MessageFormat.format("mp{0}", i), "id", getPackageName()));
+                    win[i] = (TextView) findViewById(getResources().getIdentifier( MessageFormat.format("win{0}", i), "id", getPackageName()));
+                    draw[i] = (TextView) findViewById(getResources().getIdentifier( MessageFormat.format("draw{0}", i), "id", getPackageName()));
+                    lose[i] = (TextView) findViewById(getResources().getIdentifier( MessageFormat.format("lose{0}", i), "id", getPackageName()));
+                    pts[i] = (TextView) findViewById(getResources().getIdentifier( MessageFormat.format("pts{0}", i), "id", getPackageName()));
+
+
+                    detail[i].setText(String.valueOf(teamData.getJSONObject(i).get("playedGames")));
+                    win[i].setText(String.valueOf(teamData.getJSONObject(i).get("won")));
+                    draw[i].setText(String.valueOf(teamData.getJSONObject(i).get("draw")));
+                    lose[i].setText(String.valueOf(teamData.getJSONObject(i).get("lost")));
+                    pts[i].setText(String.valueOf(teamData.getJSONObject(i).get("points")));
+
                     Uri uri = Uri.parse(teamData.getJSONObject(i).getJSONObject("team").getString("crestUrl"));
-                    Log.d("KlasmenData", "getKlasmenData: " + teamData.getJSONObject(i).getJSONObject("team").getString("crestUrl"));
-                    GlideToVectorYou.init().with(this).load(uri, logo[i]);
+                    GlideToVectorYou.init().with(this).load(uri, logos[i]);
                 }
             } catch (JSONException e) {
                 e.printStackTrace();
